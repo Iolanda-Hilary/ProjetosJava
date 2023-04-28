b7f72f8a4c85660682d8ec0dd25341736d03e3d4
Iolanda Hilary Duarte Marques Batalha <111776556+Iolanda-Hilary@users.noreply.github.com>
2023-04-28 18:22:19 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double codi1, num1, valor1;
+		double cod2, num2, val2;
+		double total;
+		
+		 Scanner sc = new Scanner(System.in);
+	
+	codi1 = sc.nextDouble();
+	num1 = sc.nextDouble();
+	valor1 = sc.nextDouble();
+	
+	cod2 = sc.nextDouble();
+	num2 = sc.nextDouble();
+	val2 = sc.nextDouble();
+	
+	
+	total = codi1 + num2;
+	System.out.println(total);
+	}
+
+}
