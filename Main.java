b97e2c88c3a19f35859f2a38475c209ceabdedc8
Iolanda Hilary Duarte Marques Batalha <111776556+Iolanda-Hilary@users.noreply.github.com>
2023-04-29 10:12:47 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,23 +3,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		double codi1, num1, valor1;
-		double cod2, num2, val2;
-		double total;
-		
-		 Scanner sc = new Scanner(System.in);
-	
-	codi1 = sc.nextDouble();
-	num1 = sc.nextDouble();
-	valor1 = sc.nextDouble();
-	
-	cod2 = sc.nextDouble();
-	num2 = sc.nextDouble();
-	val2 = sc.nextDouble();
-	
-	
-	total = codi1 + num2;
-	System.out.println(total);
-	}
 
+		int N;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("DIGITE UM NUMERO");
+		N = sc.nextInt();
+		if (N <= 0) {
+			System.out.println("numero negativo");
+		} else {
+			System.out.println("numero positivo");
+
+		}
+		sc.close();
+
+	}
 }
